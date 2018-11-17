87c47ce0923ce3057513dd5089afe6398bcd1f07
jtorso03 <38812974+jtorso03@users.noreply.github.com>
2018-11-17 17:41:13 +0100

    moveslikeFaker
    skereee

---
+++ FirstPersonPractice1/Assets/Logic/Movement/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movement : MonoBehaviour {
+
+    protected Vector3 Direction = new Vector3();
+    protected float Speed;
+    protected float JumpForce;
+    protected float gravity = -9.8f;
+	// Use this for initialization
+	void Start () {
+		
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    virtual public void Move()
+    {
+
+    }
+}
---
+++ FirstPersonPractice1/Assets/Logic/Movement/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Movement {
+
+
+    CharacterController charRefernce;
+	// Use this for initializationpuli
+	void Start () {
+        charRefernce = this.GetComponent<CharacterController>();
+        Speed = 10f;
+        JumpForce = 500f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+    public override void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&charRefernce.isGrounded == true)
+        {
+            Direction.y = JumpForce;
+            Direction.y = Direction.y - (gravity * Time.deltaTime);
+        }
+        else
+        {
+            Direction.y = Direction.y - (gravity * Time.deltaTime);
+        }
+        if (charRefernce.isGrounded == true)
+        {
+            Direction.y = 0;
+        }
+
+        Direction = new Vector3(Input.GetAxis("Horizontal"), Direction.y, Input.GetAxis("Vertical"));
+        Direction = transform.TransformDirection(Direction);
+        Direction *= Speed;
+        charRefernce.Move(Direction * Time.deltaTime);
+    
+
+
+        base.Move();
+    }
+
+
+}
