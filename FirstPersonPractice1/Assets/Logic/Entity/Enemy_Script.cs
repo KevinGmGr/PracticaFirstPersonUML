717edc3537028934ec348fe7dbd9863043101072
jtorso03 <38812974+jtorso03@users.noreply.github.com>
2018-11-24 16:22:57 +0100

    asdfghjk

---
+++ FirstPersonPractice1/Assets/Logic/Entity/Enemy_Script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Script : Entity_Script {
+
+	public float Damage = 10f;
+
+	// Use this for initialization
+	void Start () {
+		HP = 30f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void ApplyDamage(){
+		HP -= Damage;
+		Debug.Log (HP);
+	}
+
+	public void OnCollisionEnter (Collision Col){
+		if(Col.gameObject.tag == "Bullet"){
+			ApplyDamage ();
+		}
+	}
+
+}
---
+++ FirstPersonPractice1/Assets/Logic/Entity/Entity_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity_Script : MonoBehaviour {
+
+	protected float HP;
+	protected float MaxHealth;
+	protected float Ammo;
+	protected float MaxAmmo;
+	protected float AmmoReload;
+	protected float MovAx;
+	protected float JumpAx;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	virtual public void ApplyDamage(){
+
+	}
+
+	virtual public void Death(){
+
+	}
+
+	virtual public void AddHealth(){
+
+	}
+
+	virtual public void Reload(){
+
+	}
+}
