bc1551b2a07cd5f5cd7afb8b0743d4bef1fe0e3a
jtorso03 <38812974+jtorso03@users.noreply.github.com>
2018-12-01 17:38:22 +0100

    change state turret

--- FirstPersonPractice1/Assets/Logic/Entity/Turret_Script.cs
+++ FirstPersonPractice1/Assets/Logic/Entity/Turret_Script.cs
@@ -4,32 +4,76 @@ using UnityEngine;
 
 public class Turret_Script : Enemy_Script {
 
-    enum TurretStates {Idle, Detected, Shooting};
-    TurretStates currentstate;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    enum TurretStates { Idle, Detected, Shooting };
+    TurretStates currentstate;
+    public GameObject PlayerRef;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
         switch (currentstate)
         {
             case TurretStates.Idle:
 
-            break;
+                break;
 
             case TurretStates.Detected:
 
-            break;
+                if(!Physics.Raycast(this.transform.position,  ( PlayerRef.transform.position - this.transform.position).normalized, (PlayerRef.transform.position - this.transform.position).magnitude))
+                {
+                    ChangeState(TurretStates.Shooting);
+
+                }
+                break;
 
             case TurretStates.Shooting:
 
-            break;
+                break;
 
         }
 
-	}
+        Debug.Log(currentstate);
+
+    }
+
+    private void ChangeState(TurretStates NewState) {
+
+        switch (currentstate)
+        {
+
+        }
+
+        switch (NewState)
+        {
+
+        }
+
+        currentstate = NewState;
+
+    }
+
+    private void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag =="Player")
+        {
+           PlayerRef = Col.gameObject;
+            ChangeState(TurretStates.Detected);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            PlayerRef = null;
+            ChangeState(TurretStates.Idle);
+        }
+    }
 }
